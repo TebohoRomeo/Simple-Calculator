8d351e17f2fe6e90acb5bc473b564037f26e08bf
TebohoRomeo <teboholekhalo13@gmail.com>
2019-12-12 11:47:36 +0200

    Initial commit

--- src/calculator.js
+++ src/calculator.js
@@ -1,27 +1,27 @@
-function add(... args){
+function add(...args) {
     var sum = 0;
     var i = 0;
-    while(i < args.length){
-       sum += args[i];
-       i++;
+    while (i < args.length) {
+        sum += args[i];
+        i++;
     }
     return sum;
 }
-console.log(add(1,2,3,4));
+console.log(add(1, 2, 3, 4));
 
-function multiply(...args){
-    var product = 1;
-    var i = 0;
-    while(i < args.length){
-       product *= args[i];
-       i++;
-    }
-    return product;
-    
-}
-console.log(multiply(1,2,3,4));
+// function multiply(...args){
+//     var product = 1;
+//     var i = 0;
+//     while(i < args.length){
+//        product *= args[i];
+//        i++;
+//     }
+//     return product;
+
+// }
+// console.log(multiply(1,2,3,4));
 
 
-module.exports = {
-    add, multiply
-}
+// module.exports = {
+//     add, multiply
+// }
